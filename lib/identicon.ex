8d95a2900dbb34d318f502194160ac0927f84677
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2023-04-09 15:48:01 +0200

    Identicon

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -5,6 +5,41 @@ defmodule Identicon do
     |> pick_color()
     |> build_grid()
     |> filter_odd_squares()
+    |> build_pixel_map()
+    |> draw_image()
+    |> save_image(input)
+  end
+
+  def save_image(image, input) do
+    File.write("#{input}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each(pixel_map, fn {start, stop} ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end)
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(image) do
+    %Identicon.Image{grid: grid} = image
+
+    pixel_map =
+      Enum.map(grid, fn {_item, index} ->
+        x = rem(index, 5) * 50
+        y = div(index, 5) * 50
+
+        top_left = {x, y}
+        bottom_right = {x + 50, y + 50}
+
+        {top_left, bottom_right}
+      end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(image) do
